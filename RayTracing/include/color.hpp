8b4577c90dd791860ed45052239d34810078e045
Aditya Pawar <90080188+AdityaPawar162@users.noreply.github.com>
2023-12-15 01:21:35 +0530

    Added RayTracing Files

---
+++ RayTracing/include/color.hpp
@@ -0,0 +1,17 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#include <iostream>
+#include "vec3.hpp"
+
+
+void writeColor(std::ostream& out, color pixel_color) {
+
+    out << static_cast<int>(255.999*pixel_color.x()) << ' '
+        << static_cast<int>(255.999*pixel_color.y()) << ' '
+        << static_cast<int>(255.999*pixel_color.z()) << '\n';
+
+}
+
+
+#endif /* COLOR_HPP */
